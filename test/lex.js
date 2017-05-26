8ba3ff306647050c5cc459fe3ce386fccb214ad8
Loek van Gent <loek@onepercentclub.com>
2017-05-26 14:14:13 +0200

    Add tests for tokenize

---
+++ test/lex.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var tokenize = require('../lib/lex.js'),
+    test = require('tape');
+
+test('tokenize', function(assert) {
+
+
+    assert.deepEqual(
+        tokenize("<b>{{t 'username'}}</b>"),
+        ['username']);
+
+    assert.deepEqual(
+        tokenize("<b>{{input placeholder=(t 'First name')}}</b>"),
+        ['First name']);
+
+    assert.deepEqual(
+        tokenize("<b>{{task-members title=(t 'Task members') description=(t 'Members for this task')}}</b>"),
+        ['Task members', 'Members for this task']);
+
+    assert.deepEqual(
+        tokenize("this.set('title', t`My Big Title`);"),
+        ['My Big Title']);
+
+    assert.end();
+});
